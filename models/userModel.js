16db9ef5ba669386d8f4ac9a05037c5575f18fdd
Luis Fer <pastillalc@gmail.com>
2018-12-02 19:05:09 -0400

    tercer commit

--- models/userModel.js
+++ models/userModel.js
@@ -1,11 +1,12 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
-mongoose.connect("mongodb://localhost/users");
+mongoose.connect("mongodb://localhost/music");
 
 var user_scheme = new Schema({
-    nombre:String,
-    usuario:String,
-    pass:String
-})
-var User = mongoose.model("usuario", user_scheme);
+    username: String,
+    playlists: [{id: String}]
+});
+
+var User = mongoose.model("users", user_scheme);
+
 module.exports.User = User;
