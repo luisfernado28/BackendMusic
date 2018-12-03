89e48613588832f417d5212b32506f0b3ffc0afb
ndfsa <sergiomarzana1@gmail.com>
2018-12-02 20:03:49 -0400

    cuarto commit

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,61 +1,36 @@
 var User = require('../models/userModel').User;
 
-var service = require('../services/authService')
+var service = require('../services/authService');
 
-function login(req,res){
-    User.findOne({usuario: req.body.user}, (err,user)=>{
-        if(err) return res.status(500).send({mensaje:err});
-        if(!user) return res.status(500).send({mensaje:'Usuario no encontrado'});
-        service.createToken
-        res.status(200).send({token:service.createToken(user)});
-    })
-}
-function obtenerUsuarios(req,res){
-    User.find({}, (err,user)=>{
-        if(err) return res.status(500).send({mensaje: err});
+function getUsers(req, res) {
+    User.find({}, (err, user) => {
+        if (err) return res.status(500).send({message: err});
         res.status(200).send(user);
     })
 }
-function insert(req,res){
-    var user = new User({
-        nombre: req.body.nombre,
-        usuario:req.body.usuario,
-        pass: req.body.pass
-    })
-    user.save().then((us)=>{
-        console.log(JSON.stringify(us))
-        res.send(us)
-    },(err)=>{
-        console.log(JSON.stringify(err))
-        res.send(err)
-    })
-}
-function deleteUsuario(req,res){
-    User.deleteOne({nombre: req.params.nombre}, (err,user)=>{
-        if(err) return res.status(500).send({mensaje: err});
-        res.status(200).send({mensaje:"ok"});
-    })
-}
-function updateUsuario(req,res){
 
-    User.findOne({nombre: req.params.nombre},(err,user)=>{
-        user.nombre = req.body.nombre;
-        user.usuario = req.body.usuario;
-        user.pass = req.body.pass;
-        user.save().then((us)=>{
-            console.log(JSON.stringify(us))
-            res.send(us)
-        },(err)=>{
-            console.log(JSON.stringify(err))
-            res.send(err)
-        });
-    })
-
+function insertUser(req, res) {
+    let user = new User({username: req.body.username, playlists: []});
+    user.save().then((innerUser) => {
+        console.log(JSON.stringify(innerUser));
+        res.send(innerUser);
+    }, (err) => {
+        if (err){
+            console.log(JSON.stringify(err));
+            res.send(err);
+        }
+    });
 }
+
+function editUser(req, res){
+    //TODO edit users
+}
+
+function deleteUser(req, res){
+    //TODO delete users
+}
+
 module.exports = {
-    login,
-    insert,
-    obtenerUsuarios,
-    deleteUsuario,
-    updateUsuario
-}
+    getUsers,
+    insertUser
+};
--- routes/user-route.js
+++ routes/user-route.js
@@ -1,12 +1,10 @@
-var express= require('express')
+var express= require('express');
 
-var userCtrl = require('../controllers/userController')
-var auth = require('../middlewares/auth')
+var userCtrl = require('../controllers/userController');
+var auth = require('../middlewares/auth');
 var api = express.Router();
 
-api.post("/insert", userCtrl.insert);
-api.post("/login", userCtrl.login);
-api.get("/todos", userCtrl.obtenerUsuarios);
-api.delete("/delete/:nombre", userCtrl.deleteUsuario);
-api.put("/modificar/:nombre", userCtrl.updateUsuario);
+api.get("/all", userCtrl.getUsers);
+
+
 module.exports = api;
